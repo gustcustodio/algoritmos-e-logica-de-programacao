5b21a0b05dba54d3fbb3294949c7017a4c43f756
gustcustodio <gustcustodio@gmail.com>
2024-08-12 07:00:07 -0300

    pasta inicio + exemplos

---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/inicio/comentario.js
@@ -0,0 +1,13 @@
+// console.log é usado para exibir algo na tela do computador.
+console.log("Exemplo 1");
+console.log("Exemplo 2"); // mais um exemplo de console.log
+// console.log(`Exemplo 3`);
+
+/* 
+* Esse é um 
+* comentário
+* de múltiplas
+* linhas!
+*/
+
+console.log(`Exemplo 3`);
---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/inicio/sentenca_2.js
@@ -0,0 +1,3 @@
+console.log("Exemplo 1");
+console.log('Exemplo 2');
+console.log(`Exemplo 3`);
