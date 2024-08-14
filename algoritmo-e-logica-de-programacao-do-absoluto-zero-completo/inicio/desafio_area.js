f303dedf0038e01fd850ea82530f7ae00ff5ba45
gustcustodio <gustcustodio@gmail.com>
2024-08-14 07:01:40 -0300

    desafios

---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/inicio/desafio_area.js
@@ -0,0 +1,9 @@
+// PI = 3.141592 - Constantes com letras maiúsculas
+// PI * (raio * raio)
+
+const PI = 3.141592;
+let raio = 10;
+let resultado = PI * raio * raio;
+// resultado = Math.PI * raio * raio;
+
+console.log("O resultado da área é " + resultado.toFixed(2) + "m2.");
