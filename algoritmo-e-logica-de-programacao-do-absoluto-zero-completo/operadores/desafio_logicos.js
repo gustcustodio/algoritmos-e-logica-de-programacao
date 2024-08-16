1fea02687da06bdc6a2a11382320ef650f054ae3
gustcustodio <gustcustodio@gmail.com>
2024-08-16 06:53:27 -0300

    exemplos e desafios

---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/operadores/desafio_logicos.js
@@ -0,0 +1,13 @@
+// Duas expressões que vão resultar em TRUE (Aritméticos/Relacionais/Lógicos)
+let verdadeira1 = 2 + 2 >= 4 && 2 - 2 <= 0;
+let verdadeira2 = 5 + 5 <= 10 || 5 + 5 < 15;
+
+console.log(verdadeira1);
+console.log(verdadeira2)
+
+// Duas expressões que vão resultar em FALSE (Aritméticos/Relacionais/Lógicos)
+let falso1 = 2 + 2 >= 4 && 2 + 2 > 5;
+let falso2 = 5 + 6 <= 10 || 5 + 6 > 15;
+
+console.log(falso1);
+console.log(falso2);
