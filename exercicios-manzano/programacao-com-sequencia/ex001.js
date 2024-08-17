caf979ec1004f338b824a0e0089509db5ee556f0
gustcustodio <gustcustodio@gmail.com>
2024-08-17 06:57:45 -0300

    instalações e atualizações

---
+++ exercicios-manzano/programacao-com-sequencia/ex001.js
@@ -0,0 +1,4 @@
+//todo Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é F +-- (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+
+const prompt = require("prompt-sync")();
+const temperatura = Number(prompt("Temperatura em graus Celsius: "));
