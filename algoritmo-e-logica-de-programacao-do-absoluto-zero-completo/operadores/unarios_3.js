a01bafdb3cd3d8646c9c1be2d413b6630a68d9a7
gustcustodio <gustcustodio@gmail.com>
2024-08-17 06:33:44 -0300

    exemplos e desafios

---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/operadores/unarios_3.js
@@ -0,0 +1,15 @@
+console.log(!true);
+console.log(!!true);
+console.log(!false);
+console.log(!!false);
+
+console.log("-----")
+
+console.log(!"Texto");
+console.log(!"");
+
+console.log("-----")
+
+console.log(!0);
+console.log(!1);
+
