8dcb3c3ee5cf899fbdfe7eb476f36198e3e4d90a
gustcustodio <gustcustodio@gmail.com>
2024-08-23 06:59:28 -0300

    estruturas de controle (if e if/else)

---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/controle/desafio_if.js
@@ -0,0 +1,25 @@
+const nota = 10;
+
+if (nota >= 9 && nota <= 10) {
+  console.log("A");
+}
+
+if (nota >= 7 && nota <= 8.9) {
+  console.log("B");
+}
+
+if (nota >= 5 && nota <= 6.9) {
+  console.log("C");
+}
+
+if (nota >= 4.5 && nota <= 4.9) {
+  console.log("D");
+}
+
+if (nota < 4.5 && nota >= 0) {
+  console.log("F");
+}
+
+if (nota < 0 || nota > 10) {
+  console.log("Valor inválido!");
+}
---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/controle/if_4.js
@@ -0,0 +1,10 @@
+const nota = 9.3;
+
+if (nota >= 9); //* Exemplo de erro de execução
+
+{
+  console.log("Quadro de honra!");
+  console.log("Parabéns!");
+}
+
+console.log("Fim!");
