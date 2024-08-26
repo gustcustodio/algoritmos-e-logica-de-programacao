db4aba415348385bf11bf2c3b71b5752d8697210
gustcustodio <gustcustodio@gmail.com>
2024-08-26 06:56:01 -0300

    Create ex013.js

---
+++ exercicios-manzano/programacao-com-decisao/ex013.js
@@ -0,0 +1,3 @@
+/**
+ * todo Efetuar a leitura de um nome (variável NOME) e o sexo (variável SEXO) de uma pessoa e apresentar como saída uma das seguintes mensagens: "llmo. Sr.", caso seja informado o sexo masculino (utilizar como valor o caractere "M"), ou "llma. Sra.", caso seja informado o sexo feminino (utilizar como valor o caractere "F"). Após a mensagem de saudação, apresentar o nome informado. O programa deve, após a entrada do sexo, verificar primeiramente se o sexo fornecido é realmente váli do, ou seja, se é igual a "M" ou a "F". Não sendo essa condição verdadeira, o programa deve apresentar a mensagem "Sexo informado inválido".
+ */
