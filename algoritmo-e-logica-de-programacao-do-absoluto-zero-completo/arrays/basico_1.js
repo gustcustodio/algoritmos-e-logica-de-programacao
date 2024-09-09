ad45ec11bf2cd8e4233df3a16fe759286fd8df18
gustcustodio <gustcustodio@gmail.com>
2024-09-09 06:23:54 -0300

    arrays - básico

---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/arrays/basico_1.js
@@ -0,0 +1,12 @@
+//              0    1    2    3    4    5
+const notas = [7.8, 6.7, 5.2, 9.5, 7.8, 5.8];
+
+// notas = "teste";
+// notas = [1, 2, 3];
+
+console.log(notas);
+console.log(notas[3]);
+
+notas[4] = 8.7;
+
+console.log(notas);
---
+++ algoritmo-e-logica-de-programacao-do-absoluto-zero-completo/arrays/basico_2.js
@@ -0,0 +1,11 @@
+const nomes = [];
+// console.log(typeof nomes);
+
+nomes[0] = "Pedro";
+nomes[1] = "Ana";
+nomes[2] = "Bia";
+nomes[3] = "Gui";
+nomes[4] = "Rebeca";
+nomes[10] = "Tiago"; // ! adding elements with high indexes can create undefined "holes" in an array
+
+console.log(nomes);
