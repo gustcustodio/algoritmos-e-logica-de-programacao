3c3855ca5f08217d123d1d24698200fbf7bb30cb
gustcustodio <gustcustodio@gmail.com>
2024-09-15 17:45:19 -0300

    dados_3

---
+++ algoritmos-e-logica-de-programacao-o-curso-completo/secao 9 - linguagem C/tipos-de-dados/dados_3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    // int x, y;
+    // x = 5;
+    // y = 2 * x;
+    // printf("%d\n", x);
+    // printf("%d\n", y);
+
+    // int x;
+    // double y;
+    // x = 5;
+    // y = 2 * x;
+    // printf("%d\n", x);
+    // printf("%.1lf\n", y);
+
+    // double b1, b2, h, area;
+    // b1 = 6.0;
+    // b2 = 8.0;
+    // h = 5.0;
+    // area = (b1 + b2) / 2.0 * h;
+    // printf("%lf\n", area);
+
+    int a, b;
+    double resultado;
+    a = 5;
+    b = 2;
+    resultado = (double)a / b;
+    printf("%lf\n", resultado);
+
+    // double a;
+    // int b;
+    // a = 5.0;
+    // b = (int)a;
+    // printf("%d\n", b);
+
+    return 0;
+}
