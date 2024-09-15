d37f8ed8e5fae5f6d0d2af0d9f0033e595558ffc
gustcustodio <gustcustodio@gmail.com>
2024-09-15 13:40:56 -0300

    exercício resolvido

--- exercicios-manzano/programacao-com-arrays/ex002.js
+++ exercicios-manzano/programacao-com-arrays/ex002.js
@@ -1,5 +1,5 @@
 /**
- * todo Elaborar um programa que leia oito elementos numéricos inteiros em uma matriz A de uma dimensão do tipo vetor. Construir uma matriz B de mesma dimensão e tipo com os elementos da matriz A multiplicados por 5. Montar uma rotina de pesquisa binária, para pesquisar os elementos armazenados na matriz B.
+ * todo Elaborar um programa que leia oito elementos inteiros em uma matriz A do tipo vetor. Construir uma matriz B de mesma dimensão com os elementos da matriz A multiplicados por 3. O elemento B[1] deve ser implicado pelo elemento A[1] * 3, o elemento B[2] implicado pelo elemento A[2] * 3 e assim por diante, até B. Apresentar a matriz B.
  */
 
 const prompt = require("prompt-sync")();
@@ -7,10 +7,12 @@ const A = [];
 const B = [];
 
 for (let i = 0; i < 8; i++) {
-    let numero = Number(prompt(`Informe o ${i + 1}º número: `));
-    A.push(numero);
+  const numero = Number(prompt(`Informe o ${i + 1}º número: `));
+  A.push(numero);
 }
 
 for (let i = 0; i < A.length; i++) {
-    B.push(A[i] * 5);
+  B.push(A[i] * 3);
 }
+
+console.log(B);
