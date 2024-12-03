2aa3ab74f07bf2782e88f54c9ba6ef866c1bd829
gustcustodio <gustcustodio@gmail.com>
2024-12-03 07:04:47 -0300

    exercício criado

---
+++ exercicios-manzano/odernacao-e-busca/ex005.js
@@ -0,0 +1,8 @@
+/**
+ * todo Escrever um programa que leia duas matrizes A e B do tipo vetor com elementos do tipo cadeia, sendo a matriz A com 20 elementos e a matriz B com 30 elementos. Construir uma matriz C, sendo esta a junção das matrizes A e B. Desta forma, a matriz C deve ter a capacidade de armazenar 50 elementos. Apresentar os elementos da matriz C em ordem descendente.
+ */
+
+const prompt = require("prompt-sync")();
+const A = [];
+const B = [];
+const C = [];
