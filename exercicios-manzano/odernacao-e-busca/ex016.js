e2b737bc4f1c884df8f07d3bb4d5a50f62c7bef1
gustcustodio <gustcustodio@gmail.com>
2024-12-08 13:53:32 -0300

    exercício criado

---
+++ exercicios-manzano/odernacao-e-busca/ex016.js
@@ -0,0 +1,4 @@
+/**
+ * todo Elaborar um programa que leia uma matriz A com dez elementos do tipo cadeia. Construir uma matriz B de mesma dimensão e tipo que a matriz A. O último elemento da matriz A deve ser o primeiro da matriz B, o penúltimo elemento da matriz A deve ser o segundo da matriz B até que o primeiro elemento da matriz A seja o último da matriz B. Apresentar os elementos da matriz B de forma ordenada ascendente.
+ */
+
